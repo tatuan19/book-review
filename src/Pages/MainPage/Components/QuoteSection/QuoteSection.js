f3520aa67ba47c09872b21c7eb1d935e33cf8db9
chien <chiendoanngoc1705@gmail.com>
2021-06-09 10:06:50 +0700

    add Quote Section component

---
+++ src/Pages/MainPage/Components/QuoteSection/QuoteSection.js
@@ -0,0 +1,45 @@
+import { Fragment } from "react";
+
+import Quote from "../../../../components/Quote/Quote";
+
+const QuoteOnHeader = (props) => {
+  return (
+    <Fragment>
+      <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+        <div
+          className="absolute top-0 w-full h-full bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${require("assets/img/books.jpg").default})`,
+          }}
+        >
+          <span
+            id="blackOverlay"
+            className="w-full h-full absolute opacity-75 bg-black"
+          ></span>
+        </div>
+        <Quote />
+        <div
+          className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+          style={{ transform: "translateZ(0)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-blueGray-200 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default QuoteOnHeader;
