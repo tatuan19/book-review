c9af86e9b67ef8ff501b029531cb03afd7e995ea
Hoang Lam <meolam1999@gmail.com>
2021-06-12 16:36:00 +0700

    rebuild book detail UI

--- src/Pages/BookDetailPage/Components/Detail.js
+++ src/Pages/BookDetailPage/Components/Detail.js
@@ -31,39 +31,39 @@ const Detail = (props) => {
   //   event.preventDefault();
   //   console.log("123");
   // };
-  
+
   return (
     <Fragment>
-      <div className="flex flex-wrap justify-center">
-        <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-          <div className="relative ">
+      <div className="text-center" style={{ marginTop: "3rem" }}>
+        <div className="items-center flex flex-wrap">
+          <div className="w-full md:w-4/12 ml-auto mr-auto px-4 flex flex-col items-center justify-center">
             <img
               alt="..."
+              className="max-h-300-px w-auto rounded-lg shadow-lg"
               src={props.book.link}
-              className="shadow-xl h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px "
             />
           </div>
-        </div>
-        <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-          {isAdminProfile && (
-            <div className="py-6 px-3 mt-32 sm:mt-0">
-              <button
-                className="bg-yellow-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={editBookHandler}
-              >
-                EDIT
-              </button>
-              <button
-                className="bg-red-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={deleteBookHandler}
-              >
-                DELETE
-              </button>
-            </div>
-          )}
-          {/* <form onSubmit={submitHandler}>
+
+          <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
+            {isAdminProfile && (
+              <div className="py-6 px-3 mt-32 sm:mt-0">
+                <button
+                  className="bg-yellow-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={editBookHandler}
+                >
+                  EDIT
+                </button>
+                <button
+                  className="bg-red-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={deleteBookHandler}
+                >
+                  DELETE
+                </button>
+              </div>
+            )}
+            {/* <form onSubmit={submitHandler}>
             <div className="py-6 mx-auto mt-32 sm:mt-0 ">
               <input
                 // value={enteredScore}
@@ -87,28 +87,59 @@ const Detail = (props) => {
               </button>
             </div>
           </form> */}
-        </div>
-        <div className="w-full lg:w-4/12 px-4 lg:order-1">
-          <div className="flex justify-center py-10 lg:pt-4 pt-8">
-            <div className="mr-4 p-3 text-center">
-              {/* <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+          </div>
+          <div className="w-full lg:w-4/12 px-4 lg:order-1">
+            <div className="flex justify-center py-10 lg:pt-4 pt-8">
+              <div className="mr-4 p-3 text-center">
+                {/* <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
                 {props.book.score}
               </span>
               <span className="text-sm text-blueGray-400">Book Score</span> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full md:w-8/12 ml-auto mr-auto px-4">
+            <div className="md:pr-12">
+              <div className="text-lightBlue-600 p-3 text-center inline-flex items-center justify-center w-10 h-10 mb-6 shadow-lg rounded-full bg-lightBlue-300">
+                <i className="fas fa-rocket text-xl"></i>
+              </div>
+              <h3 className="text-3xl font-semibold">{props.book.title}</h3>
+              <ul className="list-none mt-6">
+                <li className="py-2">
+                  <div className="flex flex-wrap justify-center">
+                    <div>
+                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-lightBlue-600 bg-lightBlue-200 mr-3">
+                        <i className="fas fa-user text-xl"></i>
+                      </span>
+                    </div>
+                    <div>
+                      <h4 className="text-xl mt-5 font-semibold">
+                        {props.book.author}
+                      </h4>
+                    </div>
+                  </div>
+                </li>
+                <li className="py-2">
+                  <div className="flex flex-wrap justify-center">
+                    <div>
+                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-lightBlue-600 bg-lightBlue-200 mr-3">
+                        <i className="fas fa-table text-l"></i>
+                      </span>
+                    </div>
+                    <div>
+                      <h4 className="text-l mt-5 font-semibold">
+                        <Link to={`/genre/${props.book.genres}`}>{props.book.genres}</Link>
+                      </h4>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+
             </div>
           </div>
         </div>
-      </div>
-      <div className="text-center" style={{ marginTop: "6rem" }}>
-        <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-          {props.book.title}
-        </h3>
-        <div className="text-xl leading-normal mt-0 mb-2 text-blueGray-500 font-bold">
-          {props.book.author}
-        </div>
-        <div className="text-base leading-normal mt-0 mb-2 text-blueGray-500">
-          <Link to={`/genre/${props.book.genres}`}>{props.book.genres}</Link>
-        </div>
+
         <div className="flex flex-wrap justify-center">
           <div className="mt-10 py-10 border-t border-blueGray-200">
             <p
@@ -119,6 +150,7 @@ const Detail = (props) => {
             </p>
           </div>
         </div>
+
       </div>
     </Fragment>
   );
